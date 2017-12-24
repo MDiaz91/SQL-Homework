ab16449e39e5aa4c961ba04dc5c59ef104774423
Mardy <mardydiaz@ymail.com>
2017-12-23 18:00:14 -0600

    new answers

--- SQL Homework script.sql
+++ SQL Homework script.sql
@@ -6,3 +6,22 @@ FROM actor;
 SELECT CONCAT(first_name, ' ', last_name)
 AS 'Actor Name'
 FROM actor;
+
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE first_name = 'JOE';
+
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE last_name LIKE '%GEN%';
+
+SELECT actor_id, first_name, last_name
+FROM actor
+WHERE last_name LIKE '%LI%'
+ORDER BY last_name, first_name;
+
+SELECT country_id, country
+FROM country
+WHERE country in ('Afghanistan', 'Bangladesh', 'China');
+
+
